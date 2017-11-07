76b4caf1c05ce506651ff79e2227f4198ed1e7da
adamja <adam_289@hotmail.com>
2017-11-07 16:13:24 +0800

    Updated

--- mega_controller/src/main.cpp
+++ mega_controller/src/main.cpp
@@ -11,6 +11,8 @@ TODO:
       https://www.arduino.cc/en/Tutorial/SerialEvent
     - Add a queue library to assist with incoming messages
       https://playground.arduino.cc/Code/QueueArray#Download
+    - Add watch dog timer feature
+    - 
 */
 
 /*******************************************************************
@@ -46,8 +48,11 @@ TODO:
 
 // SERIAL
 #define BAUD_RATE 9600          // Serial Baud Rate
-#define MSG_SPLITTER |          // Splitter to seperate topic and message within a serial message
-#define MSG_DELIMITER $         // Delimiter to determine end of serial message
+const char MSG_SPLITTER = '|';          // Splitter to seperate topic and message within a serial message
+const char MSG_END = '$';               // Symbol to determine end of serial message
+QueueArray <char> char_queue;   // Serial char buffer
+QueueArray <String> msg_queue;  // Serial message buffer
+int msg_count = 0;              // Messages in queue to be processed
 
 float board_voltage = 5;        // Arduino Mega analogue input reads at 5V
 
@@ -59,16 +64,37 @@ int manual_fan = 0;             // Default off
 int manual_solenoid = 0;        // Default off
 
 // PH VARIABLES
+float ph_setpoint = 7.0;
+float ph_hyst_min;
+float ph_hyst_max;
+float ph_hyst_setpoint;
 
 // WATER TEMP VARIABLES
 
 // AIR TEMP VARIABLES
-#define DHTTYPE DHT22           // DHT 22  (AM2302)
-DHT dht(pin_air_temp, DHTTYPE);
-float air_temp = 0.0;
-float air_humidity = 0.0;       
+#define DHTTYPE DHT22                           // DHT 22  (AM2302)
+DHT dht(pin_air_temp, DHTTYPE);                 // DHT object setup
+const int air_array_size = 10;                  // Air temp readings
+int air_temp_cursor = 0;                        // Number of readings
+float air_temp_array[air_array_size];           // Temp reading array
+float air_humidity_array[air_array_size];       // Humidity reading array
+float air_temp_last = 0.0;                      // Last temp reading value
+float air_humidity_last = 0.0;                  // Last humidity reading value
+float air_temp_avg = 0.0;                       // 
+float air_humidity_avg = 0.0;                   // 
 
 // LIGHT VARIABLES
+
+
+//FAN
+float fan_hyst = 5.0;
+float fan_high = 30.0;
+
+// WATER LEVELS
+int water_low = 0;
+int water_med = 0;
+int water_high = 0;
+bool water_filling = false;
 
 /*******************************************************************
 # FUNCTIONS                     
@@ -91,57 +117,131 @@ void pin_setup() {
 }
 
 
-void get_air_temp_humidity() {
+float calc_avg(int size, float *readings) {
+    // TODO: sort array and remove outliers and take medium value instead
+    float total = 0;
+    float avg = 0;
+	for (int i = 0; i < size; i++)
+	{
+		total += readings[i];
+	}
+    avg = total / size;
+    
+    return avg;
+}
+
+
+/*******************************************************************
+# AIR TEMP                     
+*******************************************************************/
+
+void air_temp_loop() {
+    // Fill array with readings
+    air_temp_array[air_temp_cursor] = air_temp_last;
+    air_humidity_array[air_temp_cursor] = air_humidity_last;
+    air_temp_cursor++;  // increment cursor
+    if (air_temp_cursor >= air_array_size) {
+        air_temp_avg = calc_avg(air_array_size, air_temp_array);
+        air_humidity_avg = calc_avg(air_array_size, air_humidity_array);
+        air_temp_cursor = 0;  // reset cursor
+    }
+}
+
+
+bool read_air_temp() {
     // Reading temperature or humidity takes about 250 milliseconds!
     // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-    float h = dht.readHumidity();
-    // Read temperature as Celsius
-    float t = dht.readTemperature();
+    float h = dht.readHumidity();  // Read humidity
+    float t = dht.readTemperature();  // Read temperature
   
-    // Check if any reads failed and exit early (to try again).
     if (isnan(h) || isnan(t)) {
-        Serial.println("Failed to read from DHT sensor!");
-        return;
+        return false;  // Failed to read, return false
     }
-
-    Serial.print("Humidity: "); 
-    Serial.print(h);
-    Serial.print(" %\t");
-    Serial.print("Temperature: "); 
-    Serial.print(t);
-    Serial.print(" *C ");
+    air_temp_last = h;
+    air_temp_last = t;
+    return true;
 }
 
+/*******************************************************************
+# WATER PH                    
+*******************************************************************/
 
-void get_water_ph() {
-
-}
-
-
-void get_water_temp() {
+void read_water_ph() {
 
 }
 
+/*******************************************************************
+# WATER TEMP                    
+*******************************************************************/
 
-void get_light() {
+void read_water_temp() {
 
 }
 
+/*******************************************************************
+# LIGHT                 
+*******************************************************************/
+
+void read_light() {
+
+}
+
+/*******************************************************************
+# SET PH                  
+*******************************************************************/
 
 void set_ph() {
 
 }
 
+/*******************************************************************
+# FAN                    
+*******************************************************************/
 
 void set_fan() {
 
+    air_temp_loop();
+
+    if (air_temp_avg >= fan_high) {
+        digitalWrite(pin_fan, HIGH);
+    }
+    else if (air_temp_avg <= (fan_high - fan_hyst)) {
+        digitalWrite(pin_fan, LOW);
+    }
+}
+
+/*******************************************************************
+# WATER LEVELS                    
+*******************************************************************/
+
+void read_water_levels() {
+    water_low = digitalRead(pin_water_low);
+    water_med = digitalRead(pin_water_middle);
+    water_high = digitalRead(pin_water_high);
 }
 
 
-void set_solenoid() {
+void fill_water_tank() {
 
+    read_water_levels();
+
+    if (water_low == 1) {
+        water_filling = true;
+    }
+    if (water_filling == true) {
+        if (water_med == 1) {
+            digitalWrite(pin_solenoid, LOW);
+            water_filling = false;
+        }
+        else {
+            digitalWrite(pin_solenoid, HIGH);
+        }
+    }
 }
 
+/*******************************************************************
+# MANUAL MODE                    
+*******************************************************************/
 
 void set_manual_mode() {
     // Wait for commands via serial to toggle outputs
@@ -178,22 +278,99 @@ void set_manual_mode() {
     }
 }
 
+/*******************************************************************
+# SERIAL                
+*******************************************************************/
+
 /*
   SerialEvent occurs whenever a new data comes in the hardware serial RX. This
   routine is run between each time loop() runs, so using delay inside loop can
   delay response. Multiple bytes of data may be available.
 */
 void serialEvent() {
+
     while (Serial.available()) {
-      // get the new byte:
-      char inChar = (char)Serial.read();
-      // add it to the inputString:
-      /* inputString += inChar; */
-      // if the incoming character is a newline, set a flag so the main loop can
-      // do something about it:
-      if (inChar == '\n') {
-        /* stringComplete = true; */
+      char inChar = (char)Serial.read();    // get the new byte
+
+      if (inChar == MSG_END) {
+        // msg end
+        String msg = "";
+        while (!char_queue.isEmpty ()) {
+            msg += char_queue.dequeue();
+        }
+        msg_queue.enqueue(msg);
       }
+      else {
+        char_queue.enqueue(inChar);           // add it to the char_queue
+      }
+    }
+    msg_count = msg_queue.count();
+}
+
+
+void process_msg_queue() {
+    String msg = "";
+    String topic = "";
+    String state = "";  
+    bool first = true;  // searching for the topic when true, state when false
+    
+    while (!msg_queue.isEmpty()) {
+        msg = msg_queue.dequeue();
+
+        // Separate message into topic and state
+        for (int i = 0; i < msg.length(); i++) {
+            if (msg[i] == MSG_SPLITTER) {
+              first = false;
+            }
+            else if (first == true) {
+                topic += msg[i];
+            }
+            else {
+                state += msg[i];
+            }
+        }
+
+        // Processing message
+        if (topic == "manual") {
+            if (state == "0") {
+                manual_override = 0;
+            }
+            else if (state == "1") {
+                manual_override = 1;
+            }
+        }
+        if (topic == "ph_plus") {
+            if (state == "0") {
+                manual_ph_plus = 0;
+            }
+            else if (state == "1") {
+                manual_ph_plus = 1;
+            }
+        }
+        if (topic == "ph_minus") {
+            if (state == "0") {
+                manual_ph_minus = 0;
+            }
+            else if (state == "1") {
+                manual_ph_minus = 1;
+            }
+        }
+        if (topic == "fan") {
+            if (state == "0") {
+                manual_fan = 0;
+            }
+            else if (state == "1") {
+                manual_fan = 1;
+            }
+        }
+        if (topic == "solenoid") {
+            if (state == "0") {
+                manual_solenoid = 0;
+            }
+            else if (state == "1") {
+                manual_solenoid = 1;
+            }
+        }
     }
 }
 
@@ -210,20 +387,23 @@ void setup() {
   
   void loop() {
       if (manual_override == false) {
-          get_air_temp_humidity();      // Get air temp and humidity
-          get_water_ph();               // Get water ph
-          get_water_temp();             // Get water temp
-          get_light();                  // Get light level
-          
+          if (read_air_temp()) {    // Get air temp and humidity
+            air_temp_loop();                // it new reading add to array
+          }
+          read_water_ph();               // Get water ph
+          read_water_temp();             // Get water temp
+          read_light();                  // Get light level
+          read_water_levels();
           // send_readings();                // Write sensor readings to serial
   
           set_ph();                     // Set ph
           set_fan();                    // Set fan
-          set_solenoid();               // Set solenoid
+          fill_water_tank();               // Set solenoid
       }
       else {
         set_manual_mode();              // Manual mode function
       }
 
       serialEvent();                   // Process serial received messages
+      process_msg_queue();
   }
