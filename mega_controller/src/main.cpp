fea40f85ae0bf101fd57ae37679c72c43ea17b8c
adamja <adam_289@hotmail.com>
2017-11-07 20:46:06 +0800

    Updated

--- mega_controller/src/main.cpp
+++ mega_controller/src/main.cpp
@@ -37,10 +37,10 @@ TODO:
 #define pin_light A1              // Light sensor
 
 // Outputs
-#define pin_ph_plus 45            // ph plus relay
-#define pin_ph_minus 43           // ph minus relay
-#define pin_fan 49                // Fan relay
-#define pin_solenoid 47           // Water solenoid relay
+#define pin_ph_plus 4            // ph plus relay
+#define pin_ph_minus 5           // ph minus relay
+#define pin_fan 6                // Fan relay
+#define pin_solenoid 7           // Water solenoid relay
 
 /*******************************************************************
 # VARIABLE SETUP                 
@@ -51,8 +51,8 @@ TODO:
 const char MSG_SPLITTER = '|';          // Splitter to seperate topic and message within a serial message
 const char MSG_END = '$';               // Symbol to determine end of serial message
 QueueArray <char> char_queue;   // Serial char buffer
-QueueArray <String> msg_queue;  // Serial message buffer
-int msg_count = 0;              // Messages in queue to be processed
+String topic = "";
+String msg = "";
 
 float board_voltage = 5;        // Arduino Mega analogue input reads at 5V
 
@@ -64,12 +64,21 @@ int manual_fan = 0;             // Default off
 int manual_solenoid = 0;        // Default off
 
 // PH VARIABLES
+long prev_millis = 0;
 float ph_setpoint = 7.0;
-float ph_hyst_min;
-float ph_hyst_max;
-float ph_hyst_setpoint;
+float ph_hyst_set = 2.0;
+const int ph_array_size = 10;
+int ph_cursor = 0;
+float ph_array[ph_array_size];
+float ph_last = 0.0;
+float ph_avg = 0.0;
 
 // WATER TEMP VARIABLES
+const int water_array_size = 10;
+int water_cursor = 0;
+float water_array[water_array_size];
+float water_last = 0.0;
+float water_avg = 0.0;
 
 // AIR TEMP VARIABLES
 #define DHTTYPE DHT22                           // DHT 22  (AM2302)
@@ -84,7 +93,11 @@ float air_temp_avg = 0.0;                       //
 float air_humidity_avg = 0.0;                   // 
 
 // LIGHT VARIABLES
-
+const int light_array_size = 10;
+int light_cursor = 0;
+float light_array[light_array_size];
+float light_last = 0.0;
+float light_avg = 0.0;
 
 //FAN
 float fan_hyst = 5.0;
@@ -116,7 +129,6 @@ void pin_setup() {
     pinMode(pin_solenoid, OUTPUT);
 }
 
-
 float calc_avg(int size, float *readings) {
     // TODO: sort array and remove outliers and take medium value instead
     float total = 0;
@@ -135,19 +147,6 @@ float calc_avg(int size, float *readings) {
 # AIR TEMP                     
 *******************************************************************/
 
-void air_temp_loop() {
-    // Fill array with readings
-    air_temp_array[air_temp_cursor] = air_temp_last;
-    air_humidity_array[air_temp_cursor] = air_humidity_last;
-    air_temp_cursor++;  // increment cursor
-    if (air_temp_cursor >= air_array_size) {
-        air_temp_avg = calc_avg(air_array_size, air_temp_array);
-        air_humidity_avg = calc_avg(air_array_size, air_humidity_array);
-        air_temp_cursor = 0;  // reset cursor
-    }
-}
-
-
 bool read_air_temp() {
     // Reading temperature or humidity takes about 250 milliseconds!
     // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
@@ -162,35 +161,76 @@ bool read_air_temp() {
     return true;
 }
 
-/*******************************************************************
-# WATER PH                    
-*******************************************************************/
-
-void read_water_ph() {
-
+void air_temp_loop() {
+    if (read_air_temp() == true) {
+        air_temp_array[air_temp_cursor] = air_temp_last;
+        air_humidity_array[air_temp_cursor] = air_humidity_last;
+        air_temp_cursor++;  // increment cursor
+        if (air_temp_cursor >= air_array_size) {
+            air_temp_avg = calc_avg(air_array_size, air_temp_array);
+            air_humidity_avg = calc_avg(air_array_size, air_humidity_array);
+            air_temp_cursor = 0;  // reset cursor
+        }
+    }
 }
 
 /*******************************************************************
-# WATER TEMP                    
+# WATER PH
+*******************************************************************/
+
+bool read_water_ph() {
+    ph_last = analogRead(pin_water_ph);
+    return true;
+}
+
+float convert_ph(float ph_raw) {
+    float ph = (float)ph_raw * board_voltage / 1024 / 10 ;      // Convert the analog into millivolt
+    ph = 3.5 * ph;                                              // Convert the millivolt into pH
+    return ph;
+}
+
+void ph_loop() {
+    if (read_water_ph() == true) {
+        ph_array[ph_cursor] = ph_last;
+        ph_cursor++;  // increment cursor
+        if (ph_cursor >= ph_array_size) {
+            float raw_avg = calc_avg(ph_array_size, ph_array);
+            ph_avg = convert_ph(raw_avg);
+            ph_cursor = 0;  // reset cursor
+        }
+    }
+}
+
+void set_ph() {
+    float ph_hyst_min = ph_setpoint - ph_hyst_set;
+    float ph_hyst_max = ph_setpoint + ph_hyst_set;
+
+    if (ph_avg < ph_hyst_min) {
+        // ph_plus will need to be added
+    }
+}
+
+/*******************************************************************
+# WATER TEMP
 *******************************************************************/
 
 void read_water_temp() {
 
 }
 
+void water_temp_loop() {
+
+}
+
 /*******************************************************************
-# LIGHT                 
+# LIGHT
 *******************************************************************/
 
 void read_light() {
 
 }
 
-/*******************************************************************
-# SET PH                  
-*******************************************************************/
-
-void set_ph() {
+void light_loop() {
 
 }
 
@@ -229,7 +269,7 @@ void fill_water_tank() {
         water_filling = true;
     }
     if (water_filling == true) {
-        if (water_med == 1) {
+        if (water_med == 1 || water_high == 1) {
             digitalWrite(pin_solenoid, LOW);
             water_filling = false;
         }
@@ -282,6 +322,63 @@ void set_manual_mode() {
 # SERIAL                
 *******************************************************************/
 
+void process_msg() {
+    if (topic == "manual") {
+        if (msg == "0") {
+            manual_override = 0;
+            Serial.println("Manual Mode Off");
+        }
+        else if (msg == "1") {
+            manual_override = 1;
+            Serial.println("Manual Mode On");
+        }
+    }
+    if (topic == "ph_plus") {
+        if (msg == "0") {
+            manual_ph_plus = 0;
+            Serial.println("PH Plus Off");
+        }
+        else if (msg == "1") {
+            manual_ph_plus = 1;
+            Serial.println("PH Plus On");
+        }
+    }
+    if (topic == "ph_minus") {
+        if (msg == "0") {
+            manual_ph_minus = 0;
+            Serial.println("PH Minus Off");
+        }
+        else if (msg == "1") {
+            manual_ph_minus = 1;
+            Serial.println("PH Minus On");
+        }
+    }
+    if (topic == "fan") {
+        if (msg == "0") {
+            manual_fan = 0;
+            Serial.println("Fan off");
+        }
+        else if (msg == "1") {
+            manual_fan = 1;
+            Serial.println("Fan on");
+        }
+    }
+    if (topic == "solenoid") {
+        if (msg == "0") {
+            manual_solenoid = 0;
+            Serial.println("Solenoid off");
+        }
+        else if (msg == "1") {
+            manual_solenoid = 1;
+            Serial.println("Solenoid on");
+        }
+    }
+
+    // reset the topic and msg variables
+    topic = "";
+    msg = "";
+}
+
 /*
   SerialEvent occurs whenever a new data comes in the hardware serial RX. This
   routine is run between each time loop() runs, so using delay inside loop can
@@ -292,85 +389,31 @@ void serialEvent() {
     while (Serial.available()) {
       char inChar = (char)Serial.read();    // get the new byte
 
-      if (inChar == MSG_END) {
-        // msg end
-        String msg = "";
+      if (inChar == MSG_SPLITTER) {
+        topic = "";
+        while (!char_queue.isEmpty ()) {
+            topic += char_queue.dequeue();
+        }
+        if (topic.length() > 0) {
+            Serial.print("Received topic: ");  Serial.println(topic);
+        }
+      }
+      else if (inChar == MSG_END) {
+        msg = "";
         while (!char_queue.isEmpty ()) {
             msg += char_queue.dequeue();
         }
-        msg_queue.enqueue(msg);
+        if (msg.length() > 0) {
+            //msg_queue.enqueue(msg);
+            Serial.print("Received message: ");  Serial.println(msg);
+            if (topic.length() > 0) {
+                process_msg();
+            }
+        }
       }
       else {
         char_queue.enqueue(inChar);           // add it to the char_queue
       }
-    }
-    msg_count = msg_queue.count();
-}
-
-
-void process_msg_queue() {
-    String msg = "";
-    String topic = "";
-    String state = "";  
-    bool first = true;  // searching for the topic when true, state when false
-    
-    while (!msg_queue.isEmpty()) {
-        msg = msg_queue.dequeue();
-
-        // Separate message into topic and state
-        for (int i = 0; i < msg.length(); i++) {
-            if (msg[i] == MSG_SPLITTER) {
-              first = false;
-            }
-            else if (first == true) {
-                topic += msg[i];
-            }
-            else {
-                state += msg[i];
-            }
-        }
-
-        // Processing message
-        if (topic == "manual") {
-            if (state == "0") {
-                manual_override = 0;
-            }
-            else if (state == "1") {
-                manual_override = 1;
-            }
-        }
-        if (topic == "ph_plus") {
-            if (state == "0") {
-                manual_ph_plus = 0;
-            }
-            else if (state == "1") {
-                manual_ph_plus = 1;
-            }
-        }
-        if (topic == "ph_minus") {
-            if (state == "0") {
-                manual_ph_minus = 0;
-            }
-            else if (state == "1") {
-                manual_ph_minus = 1;
-            }
-        }
-        if (topic == "fan") {
-            if (state == "0") {
-                manual_fan = 0;
-            }
-            else if (state == "1") {
-                manual_fan = 1;
-            }
-        }
-        if (topic == "solenoid") {
-            if (state == "0") {
-                manual_solenoid = 0;
-            }
-            else if (state == "1") {
-                manual_solenoid = 1;
-            }
-        }
     }
 }
 
@@ -381,29 +424,27 @@ void process_msg_queue() {
 void setup() {
     pin_setup();                  // Setup pins as inputs and outputs
     Serial.begin(BAUD_RATE);      // Start Serial
+    Serial.println("Serial Started");
     dht.begin();                  // Air Temp
   }
   
   
-  void loop() {
-      if (manual_override == false) {
-          if (read_air_temp()) {    // Get air temp and humidity
-            air_temp_loop();                // it new reading add to array
-          }
-          read_water_ph();               // Get water ph
-          read_water_temp();             // Get water temp
-          read_light();                  // Get light level
-          read_water_levels();
-          // send_readings();                // Write sensor readings to serial
+void loop() {
+    if (manual_override == 0) {
+        air_temp_loop();
+        read_water_ph();               // Get water ph
+        read_water_temp();             // Get water temp
+        read_light();                  // Get light level
+        read_water_levels();
+        // send_readings();                // Write sensor readings to serial
   
-          set_ph();                     // Set ph
-          set_fan();                    // Set fan
-          fill_water_tank();               // Set solenoid
-      }
-      else {
+        set_ph();                     // Set ph
+        set_fan();                    // Set fan
+        fill_water_tank();               // Set solenoid
+    }
+    else {
         set_manual_mode();              // Manual mode function
-      }
-
-      serialEvent();                   // Process serial received messages
-      process_msg_queue();
-  }
+    }
+    serialEvent();                   // Process serial received messages
+    delay(1);
+}
